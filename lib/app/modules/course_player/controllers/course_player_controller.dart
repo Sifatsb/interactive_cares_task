42a2457265c0434d8d5095047da0c0044ab16b67
Sifatsb <sifatsb1906@gmail.com>
2023-11-27 21:29:38 +0600

    added video video play forward & backward.

--- lib/app/modules/course_player/controllers/course_player_controller.dart
+++ lib/app/modules/course_player/controllers/course_player_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:interactive_cares_task/app/data/dummy_data/course_dummy_data.dart';
 import 'package:interactive_cares_task/app/data/dummy_data/data_controller.dart';
@@ -16,12 +17,19 @@ class CoursePlayerController extends GetxController {
   DataController dataController = Get.find();
 
   List<Module> modules = [
-    Module(title: "Module 1: Introduction", onlineVideoLink: 'https://www.youtube.com/watch?v=nXXvWXgXEYs'),
-    Module(title: "Module 2: Getting Started", onlineVideoLink: 'https://www.youtube.com/watch?v=bzhmgkuR0dU&t=12s'),
-    Module(title: "Module 3: Advanced Topics", onlineVideoLink: 'https://www.youtube.com/watch?v=9OxxU4RlCkk'),
+    Module(
+        title: "Module 1: Introduction",
+        onlineVideoLink: 'https://www.youtube.com/watch?v=nXXvWXgXEYs'),
+    Module(
+        title: "Module 2: Getting Started",
+        onlineVideoLink: 'https://www.youtube.com/watch?v=bzhmgkuR0dU&t=12s'),
+    Module(
+        title: "Module 3: Advanced Topics",
+        onlineVideoLink: 'https://www.youtube.com/watch?v=9OxxU4RlCkk'),
   ];
 
   void playVideo() {
+    debugPrint(dataController.videoUrl.value);
     youtubeController = YoutubePlayerController(
       initialVideoId:
           YoutubePlayer.convertUrlToId(dataController.videoUrl.value) ?? '',
@@ -55,15 +63,19 @@ class CoursePlayerController extends GetxController {
     String currentModule = modules[currentModuleIndex.value].title;
     Bookmark bookmark = Bookmark(module: currentModule, time: currentTime);
 
-      bookmarks.add(bookmark);
-
+    bookmarks.add(bookmark);
   }
 
   // Function to remove a bookmark
   void removeBookmark(Bookmark bookmark) {
+    bookmarks.remove(bookmark);
+  }
 
-      bookmarks.remove(bookmark);
-
+  // Function to seek to a relative time in the video
+  void seekToRelativeTime(double seconds) {
+    double newTime =
+        youtubeController.value.position.inSeconds.toDouble() + seconds;
+    youtubeController.seekTo(Duration(seconds: newTime.toInt()));
   }
 
   @override
--- lib/app/modules/course_player/views/course_player_view.dart
+++ lib/app/modules/course_player/views/course_player_view.dart
@@ -19,70 +19,108 @@ class CoursePlayerView extends GetView<CoursePlayerController> {
         backgroundColor: AppColors.primaryColor,
         title: Text(controller.dataController.title.value),
       ),
-      body: Obx(() => SingleChildScrollView(
-            child: Column(
-              children: [
-                YoutubePlayer(
-                  controller: controller.youtubeController,
-                  showVideoProgressIndicator: true,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Modules',
-                          style: Theme.of(context).textTheme.titleLarge),
-                      for (int i = 0; i < controller.modules.length; i++)
-                        ListTile(
-                          title: Text(controller.modules[i].title),
-                          tileColor: controller.currentModuleIndex.value == i
-                              ? Colors.grey[300]
-                              : null,
-                          onTap: () {
-                            controller.youtubeController
-                                .load(controller.modules[i].title);
+      body: Obx(
+        () => SingleChildScrollView(
+          child: Column(
+            children: [
+              YoutubePlayer(
+                controller: controller.youtubeController,
+                showVideoProgressIndicator: true,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    onPressed: (() => controller.seekToRelativeTime(-10)),
+                    icon: const Icon(
+                      Icons.fast_rewind_outlined,
+                      size: 30,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: (() => controller.seekToRelativeTime(10)),
+                    icon: const Icon(
+                      Icons.fast_forward_outlined,
+                      size: 30,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Modules',
+                        style: Theme.of(context).textTheme.titleLarge),
+                    for (int i = 0; i < controller.modules.length; i++)
+                      ListTile(
+                        title: Text(controller.modules[i].title),
+                        tileColor: controller.currentModuleIndex.value == i
+                            ? Colors.grey[300]
+                            : null,
+                        onTap: () {
+                          controller.youtubeController
+                              .load(controller.modules[i].title);
 
-                            controller.currentModuleIndex.value = i;
+                          controller.currentModuleIndex.value = i;
+                          controller.dataController.videoUrl.value =
+                              controller.modules[i].onlineVideoLink;
+                        },
+                      ),
+                    10.verticalSpacing,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        /// Previous Button
+                        PrimaryButton(
+                          width: 150,
+                          borderRadius: 5,
+                          onTap: () {
+                            if (controller.currentModuleIndex > 0) {
+                              controller.currentModuleIndex--;
+                              controller.youtubeController.load(controller
+                                  .modules[controller.currentModuleIndex.value]
+                                  .title);
+                              controller.dataController.videoUrl.value =
+                                  controller
+                                      .modules[
+                                          controller.currentModuleIndex.value]
+                                      .onlineVideoLink;
+                              controller.playVideo();
+                            }
                           },
+                          text: 'Previous Module',
                         ),
-                      10.verticalSpacing,
-                      /// Next Button
-                      PrimaryButton(
-                        width: 150,
-                        borderRadius: 5,
-                        onTap: () {
-                          if (controller.currentModuleIndex <
-                              controller.modules.length - 1) {
-                            controller.currentModuleIndex++;
-                            controller.youtubeController.load(controller
-                                .modules[controller.currentModuleIndex.value]
-                                .title);
-                            controller.dataController.videoUrl.value = controller.modules[controller.currentModuleIndex.value].onlineVideoLink;
-                            controller.playVideo();
-                          }
-                        },
-                        text: 'Next Module',
-                      ),
-                      10.verticalSpacing,
-                      /// Previous Button
-                      PrimaryButton(
-                        width: 150,
-                        borderRadius: 5,
-                        onTap: () {
-                          if (controller.currentModuleIndex > 0) {
-                            controller.currentModuleIndex--;
-                            controller.youtubeController.load(controller
-                                .modules[controller.currentModuleIndex.value]
-                                .title);
-                            controller.dataController.videoUrl.value = controller.modules[controller.currentModuleIndex.value].onlineVideoLink;
-                            controller.playVideo();
-                          }
-                        },
-                        text: 'Previous Module',
-                      ),
-                      10.verticalSpacing,
-                      PrimaryButton(
+
+                        /// Next Button
+                        PrimaryButton(
+                          width: 150,
+                          borderRadius: 5,
+                          onTap: () {
+                            if (controller.currentModuleIndex <
+                                controller.modules.length - 1) {
+                              controller.currentModuleIndex++;
+                              controller.youtubeController.load(controller
+                                  .modules[controller.currentModuleIndex.value]
+                                  .title);
+                              controller.dataController.videoUrl.value =
+                                  controller
+                                      .modules[
+                                          controller.currentModuleIndex.value]
+                                      .onlineVideoLink;
+                              controller.youtubeController.load(
+                                  controller.dataController.videoUrl.value);
+                              controller.playVideo();
+                            }
+                          },
+                          text: 'Next Module',
+                        ),
+                      ],
+                    ),
+                    20.verticalSpacing,
+                    Center(
+                      child: PrimaryButton(
                         width: 150,
                         borderRadius: 5,
                         onTap: () {
@@ -90,34 +128,34 @@ class CoursePlayerView extends GetView<CoursePlayerController> {
                         },
                         text: 'Bookmark',
                       ),
-                      10.verticalSpacing,
-                      Text('Bookmarks',
-                          style: Theme.of(context).textTheme.titleLarge),
-                      if (controller.bookmarks.isEmpty)
-                        const Text('No bookmarks available.'),
-                      Column(
-                        children: [
-                          for (Bookmark bookmark in controller.bookmarks)
-                            ListTile(
-                              title: Text(
-                                  'Module: ${bookmark.module} - Time: ${bookmark.time}s'),
-                              trailing: IconButton(
-                                icon: const Icon(Icons.delete),
-                                onPressed: () {
-                                  // Implement delete bookmark functionality
-                                  controller.removeBookmark(bookmark);
-                                },
-                              ),
+                    ),
+                    10.verticalSpacing,
+                    Text('Bookmarks',
+                        style: Theme.of(context).textTheme.titleLarge),
+                    if (controller.bookmarks.isEmpty)
+                      const Text('No bookmarks available.'),
+                    Column(
+                      children: [
+                        for (Bookmark bookmark in controller.bookmarks)
+                          ListTile(
+                            title: Text(
+                                'Module: ${bookmark.module} - Time: ${bookmark.time}s'),
+                            trailing: IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () {
+                                controller.removeBookmark(bookmark);
+                              },
                             ),
-                        ],
-                      ),
-
-                    ],
-                  ),
+                          ),
+                      ],
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          )),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
